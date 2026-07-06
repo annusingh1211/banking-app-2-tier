d64c60b83cb6b68795893f5e8d7b044fff1877c1
Annu-Singh-CSE <annusingh12112003@gmail.com>
2026-07-06 10:02:43 +0530

    smoke-test rds

--- smoke_test.py
+++ smoke_test.py
@@ -1,18 +1,47 @@
-import boto3, os, sys
-client=boto3.client("ssm",region_name="eu-north-1")
+import boto3 ,os,sys
+import pymysql
+client=boto3.client("ssm",region_name="eu-east-1")
+
 params={
     os.path.basename(p["Name"],p["Value"])
     for p in client.get_parameters_by_path(
-        path="/application/banking",
-        WithDecryption=True) ["Parameters"]
+        Path="/application/banking",
+        WithDecryption=True)["Parameters"]
 }
 
-required=["DB_HOST","DB_NAME","DB_PASSWORD","DB_PORT"]
-missing=[]
+required=["DB_HOST","DB_NAME","DB_USER","DB_PASSWORD","DB_PORT"]
+missing=[k for k in required if k not in params]
 
-for k 
-
+for k in required:
+    if k in params:
+        print(k , "✅")
+    else:
+        print(k , "❌")
 
 if missing:
-    print(f"Failed: {missing}")
-    sys.exit
+    print(f"Failed : {missing}")
+    sys.exit(1)
+
+
+# DB FIND banking_db and show tables
+try:
+    connection=pymysql.connect(
+        host=params["DB_HOST"],
+        user=params["DB_USER"],
+        password=params["DB_PASSWORD"],
+        port=int(params["DB_PORT"]),
+        connect_timeout=10
+    )
+
+    cur=connection.cursor()
+    cur.execute("SHOW TABLES")
+    tables=[row[0] for row in cur.fetchall()]
+    connection.close()
+    print(f"{params["DB_NAME"]}")
+    print(f"Table : {tables}")
+
+except Exception as e:
+    print("DB ERROR ❌: ",e)
+    sys.exit(1)
+
+print("✅ Smoke test Done")
